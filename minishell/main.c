bcc3be7ab7be1d5bdd554d879f0a6709ae946c21
MonsieurSingh <teghjyot@icloud.com>
2024-08-05 17:45:18 +1000

    Initial Commit

---
+++ minishell/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  minishell
+//
+//  Created by Teghjyot Singh on 5/8/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
