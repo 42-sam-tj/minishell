07417742592a4d4838bb4a27e3b0c25220e6ea14
MonsieurSingh <teghjyot@icloud.com>
2024-08-06 23:51:54 +1000

    minishell.h: data structure for storing tokens and the command table

--- minishell/minishell.h
+++ minishell/minishell.h
@@ -5,8 +5,33 @@
 //  Created by Teghjyot Singh on 6/8/2024.
 //
 
-#ifndef minishell_h
-#define minishell_h
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <string.h>
+# include <errno.h>
+# include <readline/readline.h>
+# include <stdbool.h>
 
-#endif /* minishell_h */
+typedef struct s_tokenlist {
+	char	*tokens[100];
+	int		count;
+}	t_tokenlist;
+
+typedef struct s_command {
+	char	*command;
+	char	*args[100];
+	int		arg_count;
+}	t_command;
+
+void	tokenise(char *input, t_tokenlist *tokenlist);
+void	parse(t_tokenlist *tokenlist, t_command *cmd);
+void	expand(t_command *cmd);
+void	execute(t_command *cmd);
+void	ignore_signal(void);
+
+#endif /* MINISHELL_H */
